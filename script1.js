9b31bb7de792bb7859ef3afc4b2a187e3817fce3
Sakshi Sunil Shinde <120718340+sakshient@users.noreply.github.com>
2023-01-27 02:00:33 -0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,23 @@
+let string = "";
+let button1 = document.querySelector('.button1');
+let buttons = document.querySelectorAll('.button');
+button1.addEventListener('click',(e)=>{
+  if(e.target.innerHTML == 'AC'){
+    string = ""
+    document.querySelector('input').value = string;
+  }
+});
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+   
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+    }
+  })
+})
